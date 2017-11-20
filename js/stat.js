055b53304334abb5d69a61621be00e58b769e9b1
dimkaar <dimkaar1102@gmail.com>
2017-11-20 13:13:22 +0300

    The code has been edited after mentor's check. A sort has been added.

--- js/stat.js
+++ js/stat.js
@@ -15,21 +15,46 @@ var getRandomArbitrary = function (min, max) {
   return Math.random() * (max - min) + min;
 };
 
-var drawHistogram = function (obj, times, names, ctx, maxElement) {
-  for (var i = 0; i < times.length; i++) {
-    var step = obj.histogramHeight / maxElement;
-    obj.initialY = Math.round(240 - times[i] * step);
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = obj.colorPlayer;
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomArbitrary(0.2, 1) + ')';
-    }
-    ctx.fillRect(obj.initialX + obj.indent * i, obj.initialY, obj.histogramWidth, times[i] * step);
+var drawPlot = function (obj, time, index, ctx) {
+  ctx.fillRect(obj.initialX + obj.indent * index, obj.initialY, obj.width, time * obj.step);
+};
 
-    ctx.fillStyle = '#000000';
-    ctx.fillText(Math.round(times[i]), obj.initialX + obj.indent * i, obj.initialYScore);
-    ctx.fillText(names[i], obj.initialX + obj.indent * i, obj.initialYName);
+var writeScores = function (obj, ctx, time, name, index) {
+  ctx.fillStyle = '#000000';
+  ctx.fillText(Math.round(time), obj.initialX + obj.indent * index, obj.initialYScore);
+  ctx.fillText(name, obj.initialX + obj.indent * index, obj.initialYName);
+};
+
+var sortIndexes = function (array) {
+  var indexes = [];
+  for (var i = 0; i < array.length; i++) {
+    indexes[i] = i;
   }
+  for (var currentIndex = 0; currentIndex < array.length - 1; currentIndex++) {
+    var minElement = array[currentIndex];
+    var indexCurrentElement = indexes[currentIndex];
+    for (var j = currentIndex + 1; j < array.length; j++) {
+      if (minElement > array[j]) {
+        minElement = array[j];
+        indexCurrentElement = indexes[j];
+        var mainSwap = array[currentIndex];
+        var indexSwap = indexes[currentIndex];
+        array[currentIndex] = minElement;
+        indexes[currentIndex] = indexCurrentElement;
+        array[j] = mainSwap;
+        indexes[j] = indexSwap;
+      }
+    }
+  }
+  return indexes;
+};
+
+var sortNames = function (mainArray, dependentArray) {
+  var sortedNames = [];
+  for (var i = 0; i < mainArray.length; i++) {
+    sortedNames[i] = dependentArray[mainArray[i]];
+  }
+  return sortedNames;
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -37,25 +62,44 @@ window.renderStatistics = function (ctx, names, times) {
   gradient.addColorStop(0, '#ffffff');
   gradient.addColorStop(1, '#dddddd');
 
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = gradient;
   ctx.fillRect(100, 10, 420, 270);
 
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
 
-  ctx.fillText('Ура вы победили!', 170, 30);
-  ctx.fillText('Список результатов:', 170, 50);
+  ctx.fillText('Ура вы победили!', 170, 40);
+  ctx.fillText('Список результатов:', 170, 60);
 
   var histogramParameters = {
     initialX: 150,
     initialYName: 260,
     initialYScore: 80,
     initialY: 0,
-    histogramHeight: 150,
-    histogramWidth: 40,
+    height: 150,
+    width: 40,
     indent: 90,
-    colorPlayer: 'rgba(255, 0, 0, 1)'
+    colorPlayer: 'rgba(255, 0, 0, 1)',
   };
 
-  drawHistogram(histogramParameters, times, names, ctx, getMaxElement(times));
+  console.log('Входное время: ' + times);
+  console.log('Входные имена: ' + names);
+  var indexes = sortIndexes(times);
+  var sortedNames = sortNames(indexes, names);
+  console.log('Отсортированное время: ' + times);
+  console.log('Отсортированные имена: ' + sortedNames);
+  for (var i = 0; i < times.length; i++) {
+    if (sortedNames[i] === 'Вы') {
+      ctx.fillStyle = histogramParameters.colorPlayer;
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomArbitrary(0.2, 1) + ')';
+    }
+    histogramParameters.step = histogramParameters.height / getMaxElement(times);
+    histogramParameters.initialYScore = Math.round(235 - times[i] * histogramParameters.step);
+    histogramParameters.initialY = Math.round(240 - times[i] * histogramParameters.step);
+    drawPlot(histogramParameters, times[i], i, ctx);
+    writeScores(histogramParameters, ctx, times[i], sortedNames[i], i);
+  }
 };
