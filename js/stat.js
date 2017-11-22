64c600bba3c087119c5ccd27acafdffe12f1219f
dimkaar <dimkaar1102@gmail.com>
2017-11-22 14:58:15 +0300

    The code has been tried to be optimized after mentor's check

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var histogramParameters = {
+  initialX: 150,
+  initialYName: 260,
+  initialYScore: 80,
+  initialY: 0,
+  height: 150,
+  width: 40,
+  indent: 90,
+  colorPlayer: 'rgba(255, 0, 0, 1)'
+};
+
 var getMaxElement = function (arr) {
-  var maxElement = 0;
-  for (var i = 0; i < arr.length; i++) {
-    var currentElement = arr[i];
-    if (maxElement < currentElement) {
-      maxElement = currentElement;
+  var maxElement = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (maxElement < arr[i]) {
+      maxElement = arr[i];
     }
   }
   return maxElement;
@@ -15,14 +25,14 @@ var getRandomArbitrary = function (min, max) {
   return Math.random() * (max - min) + min;
 };
 
-var drawPlot = function (obj, time, index, ctx) {
-  ctx.fillRect(obj.initialX + obj.indent * index, obj.initialY, obj.width, time * obj.step);
+var drawPlot = function (ctx, time, step, index) {
+  ctx.fillRect(histogramParameters.initialX + histogramParameters.indent * index, histogramParameters.initialY, histogramParameters.width, time * step);
 };
 
-var writeScores = function (obj, ctx, time, name, index) {
+var writeScores = function (ctx, time, name, index) {
   ctx.fillStyle = '#000000';
-  ctx.fillText(Math.round(time), obj.initialX + obj.indent * index, obj.initialYScore);
-  ctx.fillText(name, obj.initialX + obj.indent * index, obj.initialYName);
+  ctx.fillText(Math.round(time), histogramParameters.initialX + histogramParameters.indent * index, histogramParameters.initialYScore);
+  ctx.fillText(name, histogramParameters.initialX + histogramParameters.indent * index, histogramParameters.initialYName);
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -30,37 +40,25 @@ window.renderStatistics = function (ctx, names, times) {
   gradient.addColorStop(0, '#ffffff');
   gradient.addColorStop(1, '#dddddd');
 
-  ctx.fillStyle = '#000000';
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.shadowColor = '#000000';
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowBlur = 0;
   ctx.fillStyle = gradient;
   ctx.fillRect(100, 10, 420, 270);
-
+  ctx.shadowColor = 'transparent';
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 170, 40);
   ctx.fillText('Список результатов:', 170, 60);
 
-  var histogramParameters = {
-    initialX: 150,
-    initialYName: 260,
-    initialYScore: 80,
-    initialY: 0,
-    height: 150,
-    width: 40,
-    indent: 90,
-    colorPlayer: 'rgba(255, 0, 0, 1)'
-  };
+  var step = histogramParameters.height / getMaxElement(times);
   for (var i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = histogramParameters.colorPlayer;
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomArbitrary(0.2, 1) + ')';
-    }
-    histogramParameters.step = histogramParameters.height / getMaxElement(times);
-    histogramParameters.initialYScore = Math.round(235 - times[i] * histogramParameters.step);
-    histogramParameters.initialY = Math.round(240 - times[i] * histogramParameters.step);
-    drawPlot(histogramParameters, times[i], i, ctx);
-    writeScores(histogramParameters, ctx, times[i], names[i], i);
+    ctx.fillStyle = names[i] === 'Вы' ? histogramParameters.colorPlayer : ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomArbitrary(0.2, 1) + ')';
+    histogramParameters.initialYScore = Math.round(235 - times[i] * step);
+    histogramParameters.initialY = Math.round(240 - times[i] * step);
+    drawPlot(ctx, times[i], step, i);
+    writeScores(ctx, times[i], names[i], i);
   }
 };
