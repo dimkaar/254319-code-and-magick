47895fb3f8f8ec5430ec37aefb2092af59a79e36
dimkaar <dimkaar1102@gmail.com>
2017-12-01 01:21:40 +0300

    Events have been added to the code. The commit before mentor's check.

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,9 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_FAMILIES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 137)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+
 
 var wizardTemplate = document.querySelector('#similar-wizard-template').content;
 var wizards = [];
@@ -31,6 +34,26 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeSetupWindow();
+  }
+};
+
+var openSetupWindow = function () {
+  setupWindow.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closeSetupWindow = function () {
+  setupWindow.classList.add('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var submitForm = function () {
+  setupWizardForm.submit();
+};
+
 generateWizardsData(4);
 
 var fragment = document.createDocumentFragment();
@@ -41,6 +64,36 @@ var wizardList = document.querySelector('.setup-similar-list');
 wizardList.appendChild(fragment);
 
 var setupWindow = document.querySelector('.setup');
-setupWindow.classList.remove('hidden');
 var wizardSetup = document.querySelector('.setup-similar');
 wizardSetup.classList.remove('hidden');
+
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setupWindow.querySelector('.setup-close');
+var setupSubmit = setupWindow.querySelector('.setup-submit');
+var setupWizardForm = setupWindow.querySelector('.setup-wizard-form');
+
+setupOpen.addEventListener('click', openSetupWindow);
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetupWindow();
+  }
+});
+
+setupClose.addEventListener('click', closeSetupWindow);
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetupWindow();
+  }
+});
+
+setupSubmit.addEventListener('click', function () {
+  submitForm();
+});
+
+setupSubmit.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    submitForm();
+  }
+});
